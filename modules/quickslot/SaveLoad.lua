cac083521e515d4445462f00841642dc1fdbed87
Kyzderp <amurtiger224@gmail.com>
2022-06-07 16:31:23 -0700

    Move quickslot to own folder

---
+++ modules/quickslot/SaveLoad.lua
@@ -0,0 +1,10 @@
+KyzderpsDerps = KyzderpsDerps or {}
+KyzderpsDerps.QuickSlots = KyzderpsDerps.QuickSlots or {}
+local QuickSlots = KyzderpsDerps.QuickSlots
+
+---------------------------------------------------------------------
+-- Command Entry
+---------------------------------------------------------------------
+function QuickSlots.SaveWheels()
+end
+
