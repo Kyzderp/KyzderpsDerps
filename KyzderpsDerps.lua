4ba77125f92293435e5f55c5806644c9ca42bded
Kyzderp <amurtiger224@gmail.com>
2025-11-01 09:16:57 -0700

    [1.38.0] Add terms and fix typo lol

--- KyzderpsDerps.lua
+++ KyzderpsDerps.lua
@@ -5,7 +5,7 @@
 
 KyzderpsDerps = KyzderpsDerps or {}
 KyzderpsDerps.name = "KyzderpsDerps"
-KyzderpsDerps.version = "1.37.1"
+KyzderpsDerps.version = "1.38.0"
 
 -- Defaults
 local defaultOptions = {
