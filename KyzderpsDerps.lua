bc66356f1414a716b3b45a07d995b2373595dd49
Kyzeragon <amurtiger224@gmail.com>
2025-11-05 21:31:16 -0800

    [1.39.0] Bump version

--- KyzderpsDerps.lua
+++ KyzderpsDerps.lua
@@ -5,7 +5,7 @@
 
 KyzderpsDerps = KyzderpsDerps or {}
 KyzderpsDerps.name = "KyzderpsDerps"
-KyzderpsDerps.version = "1.38.2"
+KyzderpsDerps.version = "1.39.0"
 
 -- Defaults
 local defaultOptions = {
